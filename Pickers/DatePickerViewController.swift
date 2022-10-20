489ce3560824136c083aafd96addf53f90f48bd5
Dicksen Veloopillay <dicksenveloopillay@Dicksens-MacBook-Pro.local>
2022-10-19 21:37:47 -0400

    initial commit

---
+++ Pickers/DatePickerViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Pickers
+//
+//  Created by Dicksen Veloopillay on 2022-10-19.
+//
+
+import UIKit
+
+class DatePickerViewController: UIViewController {
+
+    @IBOutlet weak var datePicker: UIDatePicker!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let date = NSDate()
+        datePicker.setDate(date as Date, animated: false)
+    }
+
+    @IBAction func onButtonPressed(_ sender: UIButton) {
+        
+        let date = datePicker.date
+          let message = "The date and time you selected is \(date)"
+          let alert = UIAlertController(
+              title: "Date and Time Selected",
+              message: message,
+              preferredStyle: .alert)
+          let action = UIAlertAction(
+              title: "That's so true!",
+              style: .default,
+              handler: nil)
+          alert.addAction(action)
+          present(alert, animated: true, completion: nil)
+    }
+    
+}
+
---
+++ Pickers/SingleComponentPickerViewController.swift
@@ -0,0 +1,61 @@
+//
+//  SingleComponentPickerViewController.swift
+//  Pickers
+//
+//  Created by Dicksen Veloopillay on 2022-10-19.
+//
+
+import UIKit
+
+class SingleComponentPickerViewController: UIViewController,UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var singlePicker: UIPickerView!
+    private let characterNames = [
+        "Luke", "Leia", "Han", "Chewbacca", "Artoo",
+        "Threepio", "Lando"]
+    
+ 
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        print("Single")
+    }
+    
+    @IBAction func onButtonPressed(_ sender: UIButton) {
+        
+        let row = singlePicker.selectedRow(inComponent: 0)
+        let selected = characterNames[row]
+        let title = "You selected \(selected)!"
+        
+        let alert = UIAlertController(
+            title: title,
+            message: "Thank you for choosing",
+            preferredStyle: .alert)
+        let action = UIAlertAction(
+            title: "You're welcome",
+            style: .default,
+            handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+        
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView,
+                    numberOfRowsInComponent component: Int) -> Int {
+        return characterNames.count
+    }
+    
+    // MARK: Picker Delegate Methods
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return characterNames[row]
+    }
+
+}
